a84d7b6f4f4b6356661d91614477d51a7a297002
Firas <firasdabbabi@gmail.com>
2023-07-08 21:59:57 +0100

    adding the nav @Chat.jsx

--- Frontend/src/components/chat/Chat.jsx
+++ Frontend/src/components/chat/Chat.jsx
@@ -140,8 +140,35 @@ export default function Chat(){
     return ( 
 
         <div className="flex h-screen">
+            
+            <nav className="lg:hidden md:hidden sm:inline xs:inline fixed bg-white w-full shadow-md">
+                <div className="mx-auto w-full text-center font-bold flex flex-grow justify-between">
 
-            <div className="bg-gray-50 w-1/3 pt-4 flex flex-col">
+                    <button className="m-0 py-3 px-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+</svg>
+
+                    </button>
+
+                    <button className="m-0 py-3 px-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+</svg>
+    
+                    </button>
+
+                    <button className="m-0 py-3 px-5 text-red-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+</svg>
+
+                    </button>
+
+                </div>
+            </nav>
+
+             <div className="bg-gray-50 w-1/3 pt-4 lg:flex md:flex flex-col hidden">
                 <div className="flex-grow">
                 <Logo />
                     {Object.keys(onlinePeopleExcludingOurUser).map(userId => (
@@ -174,12 +201,12 @@ export default function Chat(){
 
             </div>  
 
-            <div className="flex flex-col bg-white w-2/3 p-2">
+            <div className="flex flex-col  w-full p-2  bg-black">
                 
-                <div className="flex-grow flex items-center">
+                <div className="flex flex-grow items-center">
                     {!selectedUserId ? (
                         <div className="text-center w-full text-gray-500 capitalize">
-                           &larr; Start a conversation with your contacts from the sidebar
+                            Start a conversation with your contacts from the sidebar
                         </div>
                     ) : (
                         <div className="relative h-full w-full">
@@ -233,7 +260,7 @@ export default function Chat(){
                 </form>
                         )}
 
-            </div>
+            </div> 
         </div>
     )
 }
